8b6e0f1c4030c57039b27e22003365c035165648
annyie681 <annyie2006@gmail.com>
2025-08-14 16:42:22 -0700

    Update Tarea_Manejo_de_Archivos_de_Datos.java

--- Tarea_Manejo_de_Archivos_de_Datos.java
+++ Tarea_Manejo_de_Archivos_de_Datos.java
@@ -1,6 +1,9 @@
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class CSV_codigos_postales {
-  String urlString = "codigos_postales_hmo.csv";
-  
+public class leerCSV {
+  public static void main(String[] args) {
+    String documento = "codigos_postales_hmo.csv";
+  }
 }
