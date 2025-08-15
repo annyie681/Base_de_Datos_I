7d3bab3fd3cc6a57a8b43fb7dd3f7a6a02516005
annyie681 <annyie2006@gmail.com>
2025-08-14 17:06:18 -0700

    Update Tarea_Manejo_de_Archivos_de_Datos.java

--- Tarea_Manejo_de_Archivos_de_Datos.java
+++ Tarea_Manejo_de_Archivos_de_Datos.java
@@ -2,9 +2,11 @@ import java.io.BufferedReader;
 
 public class leerArchivo {
   public static void main(String[] args) {
-    String documento = "";
-    try (BufferedReader leerDocumento = new BufferedReader(documento)){
-      
-    } 
+    String documento = "https://github.com/annyie681/Base_de_Datos_I/blob/main/codigos_postales_hmo.csv";
+    try {
+      BufferedReader leerDocumento = new BufferedReader(documento);    
+    } catch (Exception e) {
+      System.out.println("Error al leer el archivo.");
+        }
   }
 }
